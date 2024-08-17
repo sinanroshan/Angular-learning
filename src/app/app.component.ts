6b7777a408d489982f25a5eb2dc272477ec8a56c
sinan-roshan <sinanroshan2233@gmail.com>
2024-08-17 23:38:17 +0530

    table added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'test-repo';
+
+  userData:any[] = [
+    {
+      name:'Arun',
+      age:28,
+      phone:9874563210,
+    },
+    {
+      name:'Abu',
+      age:35,
+      phone:8546321789,
+    },
+    {
+      name:'Rushaid',
+      age:28,
+      phone:9856741230,
+    }
+]
 }
